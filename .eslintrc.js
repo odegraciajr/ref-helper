9f3eb10ae517964bb228cff4f3ea65745c250c16
Oscar De Gracia Jr <odegraciajr@gmail.com>
2021-08-11 17:03:10 +0800

    updated graders 2

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,9 +3,21 @@ module.exports = {
     es2021: true,
     node: true
   },
-  extends: ['airbnb-base'],
+  extends: ['airbnb-base', 'prettier'],
   parserOptions: {
     ecmaVersion: 12
   },
-  rules: {}
+  rules: {
+    'no-unused-vars': 1,
+    'import/no-dynamic-require': 1,
+    'no-async-promise-executor': 1,
+    'global-require': 1,
+    'comma-dangle': ['error', 'never'],
+    'max-len': [
+      'warn',
+      { code: 80, ignoreComments: true, ignoreRegExpLiterals: true }
+    ],
+    'no-bitwise': ['error', { allow: ['~'] }],
+    'prefer-destructuring': ['error', { object: true, array: false }]
+  }
 };
